00cfa4f5378525315eea361e776be633737d4340
Sarah <sjbrooks305@gmail.com>
2020-03-18 23:58:49 -0400

    separated out css from memory card game; added popup alert upon winner feature; updated fonts on memory card game page and index to make sure there are backups

--- Memory-Card-Game/memory-card-game.js
+++ Memory-Card-Game/memory-card-game.js
@@ -1,23 +1,28 @@
 window.onload = function () {
     
-    var resetButton = document.querySelector("#resetButton");
-    var gamespace = document.getElementById("gamespace");
-    var cards = document.querySelectorAll(".thecard");
-    var blockEventDiv = document.querySelector(".blockdiv");
+    let resetButton = document.querySelectorAll(".resetButton");
+    let gamespace = document.getElementById("gamespace");
+    let cards = document.querySelectorAll(".thecard");
+    let blockEventDiv = document.querySelector(".blockdiv");
+    let winningReset = document.querySelector('#winning-page-button');
+    let winnerMessage = document.querySelector('#winner-message');
+    let attemptCounter = document.querySelectorAll('.counter');
+    let bestGame = document.getElementsByClassName('best-game');
 
     blockEventDiv.addEventListener("click", function() {
         return false;
     });
 
-    var attemptCount = 0;
-    var matchCount = 0;
+    let attemptCount = 0;
+    let matchCount = 0;
     
-    document.getElementById('best-game').innerText = JSON.parse(localStorage.getItem("best-game"));
+    bestGame[0].innerText = JSON.parse(localStorage.getItem("best-game"));
+    bestGame[1].innerText = JSON.parse(localStorage.getItem("best-game"));
 
     function cardClicked () {
         this.classList.toggle("faceup");
-        var cardName = this.getAttribute('name');
-        var faceupCards = document.querySelectorAll("div.faceup:not(.matched)");
+        let cardName = this.getAttribute('name');
+        let faceupCards = document.querySelectorAll("div.faceup:not(.matched)");
         if (faceupCards.length === 1) faceupCards[0].removeEventListener("click", cardClicked);
         if (faceupCards.length === 2) {
             if (faceupCards[0].getAttribute('name') === faceupCards[1].getAttribute('name')) {
@@ -27,24 +32,28 @@ window.onload = function () {
                 faceupCards[1].removeEventListener("click", cardClicked);
                 
                 attemptCount++;
-                document.getElementById('counter').innerText = attemptCount;
+                attemptCounter[0].innerText = attemptCount;
+                attemptCounter[1].innerText = attemptCount;
                 
                 matchCount++;
                 if (matchCount === 12) {
                     matchCount = 0;
                     setTimeout(function () {
-                        alert("Winner!");
+                        winnerMessage.style.display = 'block';
+                        // alert("Winner!");
                     }, 500);
                     if (JSON.parse(localStorage.getItem("best-game")) > attemptCount || JSON.parse(localStorage.getItem("best-game"))==null) {
                         localStorage.setItem("best-game", JSON.stringify(attemptCount));
-                        document.getElementById('best-game').innerText = JSON.parse(localStorage.getItem("best-game"));    
+                        bestGame[0].innerText = JSON.parse(localStorage.getItem("best-game"));
+                        bestGame[1].innerText = JSON.parse(localStorage.getItem("best-game"));   
                     }
                 }
             } else {
                 blockEventDiv.classList.toggle("on");
 
                 attemptCount++;
-                document.getElementById('counter').innerText = attemptCount;
+                attemptCounter[0].innerText = attemptCount;
+                attemptCounter[1].innerText = attemptCount;
 
                 setTimeout(function() {
                     faceupCards[0].classList.toggle("faceup");
@@ -62,12 +71,12 @@ window.onload = function () {
 
     function shuffle(){
         for (let i=0; i<50; i++) {
-            var shuffleCardName = "card" + (Math.ceil(Math.random()*24));
-            var cardToBeShuffled = document.getElementById(shuffleCardName).parentElement;
+            let shuffleCardName = "card" + (Math.ceil(Math.random()*24));
+            let cardToBeShuffled = document.getElementById(shuffleCardName).parentElement;
             gamespace.appendChild(cardToBeShuffled);
         }
         for (let i=0; i<cards.length; i++) {
-            var card = cards[i];
+            let card = cards[i];
             card.addEventListener("click", cardClicked);
             card.classList.remove("matched");
         }
@@ -75,16 +84,31 @@ window.onload = function () {
 
     shuffle();
 
-    resetButton.addEventListener("click", function(){
-        var faceupCards = document.querySelectorAll("div.faceup");
+    // resetButton.addEventListener("click", function() {
+    //     let faceupCards = document.querySelectorAll("div.faceup");
+    //     attemptCount = 0;
+    //     document.querySelectorAll('.counter').innerText = attemptCount;
+    //     for (let i=0; i<faceupCards.length; i++) {
+    //         let card = faceupCards[i];
+    //         card.classList.toggle("faceup");
+    //     }
+    //     shuffle();
+    // });
+
+    $('body').on('click', '.resetButton', function() {
+        let faceupCards = document.querySelectorAll("div.faceup");
         attemptCount = 0;
-        document.getElementById('counter').innerText = attemptCount;
+        attemptCounter[0].innerText = attemptCount;
+        attemptCounter[1].innerText = attemptCount;
         for (let i=0; i<faceupCards.length; i++) {
-            var card = faceupCards[i];
+            let card = faceupCards[i];
             card.classList.toggle("faceup");
         }
         shuffle();
     });
 
+    winningReset.addEventListener("click", function() {
+        winnerMessage.style.display = 'none';
+    });
 
 }
